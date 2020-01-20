3ee44e866226e9147398bffde14b04e43060e68e
shivam gupta <shivamg8797@gmail.com>
2020-01-20 18:05:44 +0530

    permission cards

--- app.js
+++ app.js
@@ -2,6 +2,29 @@ Vue.component("notification", {
   props: ["step", "brand"],
   template: "#notification"
 });
+Vue.component("multi-tab", {
+  template: "#multi-tab"
+});
+Vue.component("calling-card", {
+  props: {
+    clientname: String,
+    heading: String,
+    loader: Boolean,
+    incoming: Boolean,
+    calling: Boolean,
+    connecting: Boolean,
+    connectingError: String
+  },
+  template: "#calling-card"
+});
+Vue.component("permission-card", {
+  props: {
+    heading: String,
+    type: String,
+    icon: Boolean
+  },
+  template: "#permission-card"
+});
 new Vue({
   el: "#app"
 });
